b232e1154179d09474b485bd343ded872905bbf2
kwangwoonInfocont <winnie278@nate.com>
2020-11-21 14:46:52 +0900

    입학 메뉴추가

--- src/components/tabMenu.vue
+++ src/components/tabMenu.vue
@@ -6,10 +6,11 @@
            <span>정보콘텐츠학과</span>
         </div>
         <div class = "nav_menu">
-            <router-link to="/">학과안내</router-link>
-            <router-link to="/about">학사행정</router-link>
-            <router-link to="/about">커리큘럼</router-link>
-            <router-link to="/about">커뮤니티</router-link>
+            <router-link to="/info">학과안내</router-link>
+            <router-link to="/bachelor">학사행정</router-link>
+            <router-link to="/curri">커리큘럼</router-link>
+            <router-link to="/community">커뮤니티</router-link>
+            <router-link to="/apply">입학</router-link>
         </div>
       </div>
     </div>
