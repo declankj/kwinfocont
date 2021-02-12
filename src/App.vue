2e33a9658ba687bee58f19745d676cad099dad72
minsu.rob <minsu.rob@gmail.com>
2021-02-12 23:48:21 +0900

    feat : update main font

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,10 @@ export default {
 
 </script>
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@500&display=swap');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans KR', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
