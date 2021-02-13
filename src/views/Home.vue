e6dadddae8a2f1b9289939a9118ac508a9176908
minsu.rob <minsu.rob@gmail.com>
2021-02-14 01:58:26 +0900

    슬라이더 이미지 임시 교체

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,10 @@
     <div id="app">
       <Slider animation="fade" v-model="sliderValue" :interval="20000" :duration="5000" :speed="1000" style="height:400px">
         <SliderItem v-for="(i, index) in list" :key="index" @click="changeIndex(1);" :style="i">
-          <p style="line-height: 280px; font-size: 5rem; text-align: center;">
-            Page  {{i.backgroundColor}}
+          <!--<img :src="i.path">-->
+          <img :src="i.path">
+              <p style="line-height: 280px; font-size: 5rem; text-align: center;">
+            광운대학교  {{i.backgroundColor}}
           </p>
         </SliderItem>
       </Slider>
@@ -70,17 +72,42 @@ export default {
       () =>
         (this.list = [
           {
-            backgroundColor: '#3f51b5',
+            path: require('../assets/img/pegasus-big.png'),
             width: '100%',
-            height: '700px'
+            height: '700px',
+            background: 'linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)',
+            backgroundSize: '400% 400%',
+            animation: 'gradient 20s ease infinite'
           },
           {
-            backgroundColor: '#eee',
+            path: require('../assets/img/pegasus-big.png'),
             width: '100%',
-            height: '100%'
+            height: '100%',
+            background: 'linear-gradient(-45deg, #ee7752, #8f4643, #7e051f, #23d5ab)',
+            backgroundSize: '400% 400%',
+            animation: 'gradient 10s ease infinite',
+            color: 'white'
           }
         ]), 10
     )
   }
 }
 </script>
+<style>
+#app {
+            background1: linear-gradient(-45deg, #7a0501, #e73c7e, #ffffff, #7e051f);
+            background2: linear-gradient(-45deg, #ee7752, #e73c7e, #7e051f, #23d5ab);
+}
+
+@keyframes gradient {
+0% {
+background-position: 0% 50%;
+}
+50% {
+background-position: 100% 50%;
+}
+100% {
+background-position: 0% 50%;
+}
+}
+</style>
