a49124f1558d0f0182bdaa0480a6ead9ffca8973
minsu.rob <minsu.rob@gmail.com>
2021-02-22 00:22:04 +0900

    apply list css

--- src/views/Curriculum.vue
+++ src/views/Curriculum.vue
@@ -1,3 +1,71 @@
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+/* Create three columns of equal width */
+.columns {
+  float: left;
+  width: 33.3%;
+  padding: 8px;
+}
+
+/* Style the list */
+.price {
+  list-style-type: none;
+  border: 1px solid #eee;
+  margin: 0;
+  padding: 0;
+  -webkit-transition: 0.3s;
+  transition: 0.3s;
+}
+
+/* Add shadows on hover */
+.price:hover {
+  box-shadow: 0 8px 12px 0 rgba(0,0,0,0.2)
+}
+
+/* Pricing header */
+.price .header {
+  background-color: #77222d;
+  color: white;
+  font-size: 25px;
+}
+
+/* List items */
+.price li {
+  border-bottom: 1px solid #eee;
+  padding: 20px;
+  text-align: center;
+}
+
+/* Grey list item */
+.price .greyList {
+  background-color: #eee;
+  font-size: 20px;
+}
+
+/* The "Sign Up" button */
+.button {
+  background-color: #77222d;
+  border: none;
+  color: white;
+  padding: 10px 25px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 18px;
+}
+
+/* Change the width of the three columns to 100%
+(to stack horizontally on small screens) */
+@media only screen and (max-width: 600px) {
+  .columns {
+    width: 100%;
+  }
+}
+
+</style>
+
 <template>
   <v-container fluid>
     <v-flex xs12 class="text-xs-center display-1 font-weight-black my-2"
@@ -15,9 +83,19 @@
         :key="item.title"
         :value="'tab-' + idx"
       >
-        <v-card flat height="400px">
+        <v-card flat>
           <v-card-title primary-title>
-                <table id="list">
+                        <div v-for="curriculum in item.curriculums" :key="curriculum.subject" class="columns">
+                          <ul class="price" >
+                              <li class="header">{{curriculum.subject}}</li>
+                              <li class="greyList">{{curriculum.professor}}</li>
+                              <li>{{curriculum.day}}</li>
+                              <li>{{curriculum.semester}}</li>
+                              <li class="greyList"><a href="#" class="button">상세정보</a></li>
+                          </ul>
+                        </div>
+
+                <!--<table id="list">
                     <thead>
                         <tr>
                             <th>과목</th>
@@ -34,7 +112,7 @@
                             <td>{{curriculum.semester}}</td>
                         </tr>
                     </tbody>
-                </table>
+                </table>!-->
           </v-card-title>
         </v-card>
       </v-tab-item>
