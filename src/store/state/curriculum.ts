841ea99ad8ef1e9ba0cf0a565476407d03ba67b5
minsu.rob <minsu.rob@gmail.com>
2021-04-04 02:13:07 +0900

    update curriculum data

--- src/store/state/curriculum.ts
+++ src/store/state/curriculum.ts
@@ -25,30 +25,287 @@ const curriculumState = {
         professor: '홍길동',
         day: '토요일',
         semester: '1학기',
-        address: 1
+        credits: 3
       },
       {
         subject: '현대사회와 윤리',
         professor: '정형원',
         day: '토요일',
         semester: '1학기',
-        address: 1
+        credits: 3
       },
       {
         subject: '창의적 아이디어 발상',
         professor: '정형원',
         day: '토요일',
         semester: '1학기',
-        address: 1
+        credits: 3
+      },
+      {
+        subject: '정보화와 현대사회',
+        professor: '정형원',
+        day: '수요일',
+        semester: '2학기',
+        credits: 3
+      },
+      {
+        subject: '컴퓨터그래픽스1',
+        professor: '심선애(프리미어)',
+        day: '토요일',
+        semester: '2학기',
+        credits: 3
+      },
+      {
+        subject: '컴퓨터그래픽스2',
+        professor: '이준희(포토샵)',
+        day: '토요일',
+        semester: '2학기',
+        credits: 3
+      },
+      {
+        subject: '현대사회와 정보보호',
+        professor: '조성수',
+        day: '토요일',
+        semester: '2학기',
+        credits: 3
       }
       ]
     },
     {
-      title: '2학년', text: '각 나라, 도시별로 특색있는 공연, 미술 관람하기'
+      title: '2학년',
+      curriculums: [{
+        subject: '인터페이스개론',
+        professor: '조수영',
+        day: '수요일',
+        semester: '1학기',
+        credits: 3
+      },
+      {
+        subject: '디자인의이해',
+        professor: '정형원',
+        day: '토요일',
+        semester: '1학기',
+        credits: 3
+      },
+      {
+        subject: '암호학개론',
+        professor: '김영희',
+        day: '토요일',
+        semester: '1학기',
+        credits: 3
+      },
+      {
+        subject: '지식정보콘텐츠',
+        professor: '심선애',
+        day: '토요일',
+        semester: '1학기',
+        credits: 3
+      },
+      {
+        subject: '프로그래밍언어',
+        professor: '문석재',
+        day: '화요일',
+        semester: '2학기',
+        credits: 3
+      },
+      {
+        subject: '프로그래밍언어',
+        professor: '문석재',
+        day: '수요일',
+        semester: '2학기',
+        credits: 3
+      },
+      {
+        subject: '논리시스템',
+        professor: '손광철',
+        day: '토요일',
+        semester: '2학기',
+        credits: 3
+      },
+      {
+        subject: '자료구조',
+        professor: '황치곤',
+        day: '토요일',
+        semester: '2학기',
+        credits: 3
+      },
+      {
+        subject: '네트워크 해킹과 보안',
+        professor: '최정현',
+        day: '토요일',
+        semester: '2학기',
+        credits: 3
+      },
+      {
+        subject: '게임학의 이해',
+        professor: '홍길동',
+        day: '이러닝',
+        semester: '2학기',
+        credits: 3
+      }
+      ]
     },
-    { title: '3학년', text: '각 나라, 도시별만의 특별한 아이템 쇼핑하기' },
     {
-      title: '4학년', text: '색다른 체험이나 자연 속에서 휴식하기'
+      title: '3학년',
+      curriculums: [{
+        subject: '콘텐츠기획개론',
+        professor: '홍길동',
+        day: '화요일',
+        semester: '1학기',
+        credits: 3
+      },
+      {
+        subject: '디지털이미지',
+        professor: '홍길동',
+        day: '수요일',
+        semester: '1학기',
+        credits: 3
+      },
+      {
+        subject: '웹프로그래밍',
+        professor: '홍길동',
+        day: '토요일',
+        semester: '1학기',
+        credits: 3
+      },
+      {
+        subject: '가상현실',
+        professor: '홍길동',
+        day: '토요일',
+        semester: '1학기',
+        credits: 3
+      },
+      {
+        subject: '전자상거래보안',
+        professor: '홍길동',
+        day: '토요일',
+        semester: '1학기',
+        credits: 3
+      },
+      {
+        subject: '게임학의 이해',
+        professor: '홍길동',
+        day: '이러닝',
+        semester: '1학기',
+        credits: 3
+      },
+      {
+        subject: '디지털스토리텔링',
+        professor: '홍길동',
+        day: '이러닝',
+        semester: '1학기',
+        credits: 3
+      },
+      {
+        subject: '시스템 해킹과 보안',
+        professor: '이창원',
+        day: '화요일',
+        semester: '2학기',
+        credits: 3
+      },
+      {
+        subject: '프로그래밍의 실습',
+        professor: '홍길동',
+        day: '수요일',
+        semester: '2학기',
+        credits: 3
+      },
+      {
+        subject: '스마트기술학',
+        professor: '정형원',
+        day: '토요일',
+        semester: '2학기',
+        credits: 3
+      },
+      {
+        subject: '콘텐츠영상처리',
+        professor: '권순철',
+        day: '토요일',
+        semester: '2학기',
+        credits: 3
+      },
+      {
+        subject: '디지털사운드',
+        professor: '홍길동',
+        day: '토요일',
+        semester: '2학기',
+        credits: 3
+      }
+      ]
+    },
+    {
+      title: '4학년',
+      curriculums: [{
+        subject: '실무프로젝트1',
+        professor: '홍길동',
+        day: '화요일',
+        semester: '1학기',
+        credits: 3
+      },
+      {
+        subject: '컴퓨터그래픽스2',
+        professor: '홍길동',
+        day: '수요일',
+        semester: '1학기',
+        credits: 3
+      },
+      {
+        subject: '객체지향 프로그래밍',
+        professor: '홍길동',
+        day: '토요일',
+        semester: '1학기',
+        credits: 3
+      },
+      {
+        subject: '사이버포렌식 실습',
+        professor: '홍길동',
+        day: '토요일',
+        semester: '1학기',
+        credits: 3
+      },
+      {
+        subject: '콘텐츠산업마케팅',
+        professor: '홍길동',
+        day: '이러닝',
+        semester: '1학기',
+        credits: 3
+      },
+      {
+        subject: '모바일앱 기획 및 제작',
+        professor: '하마처',
+        day: '수요일',
+        semester: '2학기',
+        credits: 3
+      },
+      {
+        subject: '보안정책과 관리',
+        professor: '김도형',
+        day: '토요일',
+        semester: '2학기',
+        credits: 3
+      },
+      {
+        subject: '시스템 보안실습',
+        professor: '변정수',
+        day: '토요일',
+        semester: '2학기',
+        credits: 3
+      },
+      {
+        subject: '실무프로젝트2',
+        professor: '노이한',
+        day: '토요일',
+        semester: '2학기',
+        credits: 3
+      },
+      {
+        subject: '게임디자인',
+        professor: '홍길동',
+        day: '이러닝',
+        semester: '2학기',
+        credits: 3
+      }
+      ]
     }
   ]
 }
